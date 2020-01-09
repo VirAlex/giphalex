b59b3c0b1edc88603fed78c0aa1fef103a94047d
VirAlex <alexou73@gmail.com>
2020-01-09 15:27:16 +0100

    try deploy

---
+++ src/components/GifList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Gif from './gif';
+
+const GifList = ({ gifs, selectGif }) => {
+  return (
+    <div className="gif-list">
+      {gifs.map(({ id }) => <Gif id={id} key={id} selectGif={selectGif} />)}
+    </div>
+  );
+};
+export default GifList;
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app'
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
+
+// class Hello extends Component {
+//   constructor(props){
+//     super(props);
+
+//     this.state={
+//       clicked: false
+//     };
+//   }
+
+//     handleClick = () => {
+//       this.setState({
+//         clicked: !this.state.clicked
+//       });
+//     }
+
+//   render() {
+//     return(
+//     <div className={this.state.clicked ? 'clicked' : null }
+//     onClick={this.handleClick}> Hello {this.props.name}</div>
+//     )
+//   }
+// }
+
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />,
+    root);
 }
